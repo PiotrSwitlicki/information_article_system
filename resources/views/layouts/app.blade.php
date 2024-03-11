2b6d801695f7ce281122d6b9098543f434d7b834
PiotrSwitlicki <PiotrSwitlicki2@gmail.com>
2024-03-11 23:51:58 +0100

    edycja stopki

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,9 +97,9 @@ body {
         </div>
     </main>
 
-    <footer class="footer mt-auto py-3 bg-light">
+    <footer class="footer mt-auto py-3 bg-light" style="opacity: 0.6">
         <div class="container">
-            <span class="text-muted">© 2024 Information Article System. All rights reserved.</span>
+            <span style="color: black;" class="text-muted">© 2024 Information Article System. All rights reserved.</span>
         </div>
     </footer>
     </div>
@@ -107,7 +107,6 @@ body {
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Additional JS scripts can be included here -->
 </body>
 </html>
 
